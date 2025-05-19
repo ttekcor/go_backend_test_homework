282aee70f544bc1666841e6195efb78f083c54b1
Kostya Paranko <curvhand@gmail.com>
2025-05-19 22:20:36 +1000

    include outside module

--- example/go.mod
+++ example/go.mod
@@ -4,4 +4,8 @@ go 1.24.1
 
 replace calc => ../calc
 
-require calc v0.0.0-00010101000000-000000000000 // indirect
+require (
+	calc v0.0.0-00010101000000-000000000000 // indirect
+	github.com/bojanz/currency v1.3.1 // indirect
+	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
+)
--- example/main.go
+++ example/main.go
@@ -3,8 +3,22 @@ package main
 import (
 	"calc"
 	"fmt"
+
+	"github.com/bojanz/currency"
 )
 
 func main() {
-    fmt.Println("Сумма =", calc.AddInts(1, 56, 78,-33))
+    // считаем баланс в рублях
+    sum := calc.AddInts(1, 56, 78,-33)
+
+    // вывод будет с копейками, поэтому умножаем на 100     
+    total, err := currency.NewAmountFromInt64(int64(sum * 100), "RUB")
+    if err != nil {
+        panic(err)
+    }
+    // справа от значения будет добавляться знак рубля
+    locale := currency.NewLocale("ru")
+    formatter := currency.NewFormatter(locale)
+
+    fmt.Println("Баланс:", formatter.Format(total))    
 } 
